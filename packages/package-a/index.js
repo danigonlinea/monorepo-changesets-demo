6a30122c8e61f84b7d113b0dd299b992a00ca1f8
Daniel <danigonlinea@gmail.com>
2022-04-25 16:38:04 +0200

    feat: another console

--- packages/package-a/index.js
+++ packages/package-a/index.js
@@ -1,5 +1,5 @@
 
 import { capitalize } from "package-b"
 
-console.log(capitalize("hello World, here we go!"))
+console.log(capitalize("hello World, another here we go!"))
 console.log(capitalize("hello World, another!"))
